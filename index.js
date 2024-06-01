2638f52f00135b2d6f52b08d24ac5623bae7a2fe
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-01 15:16:55 +0530

    tried out the asynchronous way of reading and writing into the file reading from start then using it to readf from append then witing these files into final.txt

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const fs=require('fs');
 
 
+//Blocking, Synchronous way
+// const txtIn =fs.readFileSync('txt/input.txt','utf-8');
+// console.log(txtIn)
+// const txtOut=`This is what we lnow about the avocado: ${txtIn}. \n Created on ${Date.now()} `;
+// fs.writeFileSync('txt/output.txt',txtOut);
+// console.log('File Written');
 
-const txtIn =fs.readFileSync('txt/input.txt','utf-8');
 
-console.log(txtIn)
-
-
-const txtOut=`This is what we lnow about the avocado: ${txtIn}. \n Created on ${Date.now()} `;
-fs.writeFileSync('txt/output.txt',txtOut);
-console.log('File Written');
+//Non-Blocking,asynchronus way
+fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`,'utf-8',(err,data3)=>{
+            console.log(data3);
+            fs.writeFile(`./txt/final.txt`,`${data2}\n${data3}`,`utf-8`,(err)=>{
+                console.log(`Your file has been written😊😊`);
+            })
+        });
+    });
+});
+console.log('Will Read File!');
