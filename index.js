f226c39983513afc6656816f0fa062eeedfaa4e4
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-01 16:41:05 +0530

    created a very basic api calling data drom data.json file present in dev-deata

--- index.js
+++ index.js
@@ -29,22 +29,31 @@ const url = require("url");
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //SERVER
+const data=fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+    const dataObj = JSON.parse(data);
+
+
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-  
-    if (pathName === '/' || pathName === '/overview') {
-      res.end('This is the OVERVIEW');
-    } else if (pathName === '/product') {
-      res.end('This is the PRODUCT');
-    } else {
-      res.writeHead(404, {
-        'Content-type': 'text/html',
-        'my-own-header': 'hello-world'
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/api") {
+      res.writeHead(200, {
+        "Content-type": "application/json",
       });
-      res.end('Page not found');
-    }
-  });
-  
-  server.listen(8000, '127.0.0.1', () => {
-    console.log('Server is listening on port 8000');
-  });
+      res.end(data);
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("Page not found");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server is listening on port 8000");
+});
