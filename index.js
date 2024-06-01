275b74243435d710eb162a274ebbc46553bca299
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-01 16:12:16 +0530

    added a very basic routing to our server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const fs=require('fs');
-const http=require('http');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -11,7 +12,6 @@ const http=require('http');
 // const txtOut=`This is what we lnow about the avocado: ${txtIn}. \n Created on ${Date.now()} `;
 // fs.writeFileSync('txt/output.txt',txtOut);
 // console.log('File Written');
-
 
 //Non-Blocking,asynchronus way
 // fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
@@ -27,14 +27,24 @@ const http=require('http');
 // });
 // console.log('Will Read File!');
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //SERVER
-const server=http.createServer((req,res)=>{
-    
-    res.end(`Hello from the server!`);
-});
-
-server.listen(8000,`127.0.0.1`,()=>{
-    console.log(`Listening to requests on port 8000`);
-});
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+  
+    if (pathName === '/' || pathName === '/overview') {
+      res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+      res.end('This is the PRODUCT');
+    } else {
+      res.writeHead(404, {
+        'Content-type': 'text/html',
+        'my-own-header': 'hello-world'
+      });
+      res.end('Page not found');
+    }
+  });
+  
+  server.listen(8000, '127.0.0.1', () => {
+    console.log('Server is listening on port 8000');
+  });
